65edbd90c6aabf1ce6847828786ff07ed5c9ab14
Marco Bazzoni <bazzoni.marco@gmail.com>
2015-04-02 17:15:36 +0200

    Added content service

---
+++ cms-content-service/src/main/java/ms/cms/content/service/config/WebConfig.java
@@ -0,0 +1,16 @@
+package ms.cms.content.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * WebConfig
+ * Created by thebaz on 23/03/15.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan({"ms.cms.content.service.web"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
---
+++ cms-content-service/src/main/java/ms/cms/content/service/web/ContentService.java
@@ -0,0 +1,13 @@
+package ms.cms.content.service.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ContentService
+ * Created by bazzoni on 02/04/2015.
+ */
+@RestController(value = "contentWebService")
+@RequestMapping(value = "/public")
+public class ContentService {
+}
