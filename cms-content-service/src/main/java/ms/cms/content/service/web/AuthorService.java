9ea358b53a3091a8d4390d9e2c8446f8f05cb39c
Marco Bazzoni <bazzoni.marco@gmail.com>
2015-04-30 22:08:39 +0200

    find content by author and search query stub

---
+++ cms-content-service/src/main/java/ms/cms/content/service/web/AuthorService.java
@@ -0,0 +1,41 @@
+package ms.cms.content.service.web;
+
+import ms.cms.data.CmsContentRepository;
+import ms.cms.domain.CmsContent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ContentService
+ * Created by bazzoni on 02/04/2015.
+ */
+@RestController(value = "searchWebService")
+@RequestMapping(value = "/api")
+public class AuthorService {
+    @Autowired
+    private CmsContentRepository contentRepository;
+
+    @Secured({"ROLE_MANAGER"})
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    HttpEntity<PagedResources<CmsContent>> contents(PagedResourcesAssembler assembler,
+                                                    @RequestParam("q") String query,
+                                                    @RequestParam(value = "page", defaultValue = "0") int page,
+                                                    @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        PageRequest pageable = new PageRequest(page, size, Sort.Direction.DESC, "modificationDate");
+        Page<CmsContent> contents = contentRepository.findAll(pageable);
+        return new ResponseEntity<>(assembler.toResource(contents), HttpStatus.OK);
+    }
+}
