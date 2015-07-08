63143d356708222d1b85f919653e91ec490e8239
Marco Bazzoni <bazzoni.marco@gmail.com>
2015-07-08 23:10:54 +0200

    Added page and menu management (UI)

---
+++ cms-authoring-common/src/main/java/sparkle/cms/authoring/common/business/PageManager.java
@@ -0,0 +1,59 @@
+package sparkle.cms.authoring.common.business;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import sparkle.cms.data.CmsPageRepository;
+import sparkle.cms.data.CmsSiteRepository;
+import sparkle.cms.domain.CmsPage;
+import sparkle.cms.domain.CmsSite;
+import sparkle.cms.domain.CmsUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static sparkle.cms.authoring.common.utils.AuthoringUtils.toPrettyURL;
+
+/**
+ * PageManager
+ * Created by bazzoni on 08/07/2015.
+ */
+@Component
+public class PageManager {
+    @Autowired
+    private CmsSiteRepository cmsSiteRepository;
+    @Autowired
+    private CmsPageRepository cmsPageRepository;
+
+    public Page<CmsPage> findAllPages(CmsUser cmsUser, Pageable pageable) {
+        List<CmsSite> cmsSites = cmsSiteRepository.findByWebMaster(cmsUser);
+        List<CmsPage> cmsPages = new ArrayList<>();
+        for (CmsSite cmsSite : cmsSites) {
+            cmsPages.addAll(cmsPageRepository.findBySiteId(cmsSite.getId()));
+        }
+        return new PageImpl<>(cmsPages, pageable, cmsPages.size());
+    }
+
+    public Page<CmsPage> findAuthoredPages(CmsUser cmsUser, Pageable pageable) {
+        CmsSite cmsSite = cmsSiteRepository.findOne(cmsUser.getAuthoredSiteId());
+        List<CmsPage> cmsPages = cmsPageRepository.findBySiteId(cmsSite.getId());
+        return new PageImpl<>(cmsPages, pageable, cmsPages.size());
+    }
+
+    public void createPage(CmsPage cmsPage) {
+        cmsPage.setName(toPrettyURL(cmsPage.getTitle()));
+        final CmsPage parent = cmsPage.getParent();
+        if (parent == null) {
+            cmsPage.setUri("/");
+        } else {
+            cmsPage.setUri(parent.getUri() + "/" + toPrettyURL(cmsPage.getTitle()));
+        }
+        cmsPageRepository.save(cmsPage);
+    }
+
+    public void deletePage(String pageId) {
+        cmsPageRepository.delete(pageId);
+    }
+}
---
+++ cms-authoring-ui/src/main/java/sparkle/cms/authoring/ui/web/PageController.java
@@ -0,0 +1,105 @@
+package sparkle.cms.authoring.ui.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import sparkle.cms.authoring.common.business.PageManager;
+import sparkle.cms.domain.CmsPage;
+import sparkle.cms.domain.CmsSite;
+import sparkle.cms.domain.CmsUser;
+import sparkle.cms.registration.common.business.RegistrationException;
+import sparkle.cms.registration.common.business.RegistrationManager;
+import sparkle.cms.registration.common.business.SiteManager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static sparkle.cms.utils.UserUtils.isAuthor;
+import static sparkle.cms.utils.UserUtils.isWebmaster;
+
+/**
+ * PageController
+ * Created by bazzoni on 08/07/2015.
+ */
+@Controller(value = "pageController")
+public class PageController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private RegistrationManager registrationManager;
+    @Autowired
+    private SiteManager siteManager;
+    @Autowired
+    private PageManager pageManager;
+
+    @ModelAttribute("allSites")
+    public List<CmsSite> allSites(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            CmsUser cmsUser = registrationManager.findUser(request.getRemoteUser());
+            if (isWebmaster(cmsUser)) {
+                return siteManager.findAllSites(cmsUser);
+            } else if (isAuthor(cmsUser)) {
+                return siteManager.findAuthoredSites(cmsUser);
+            }
+        } catch (RegistrationException e) {
+            String msg = String.format("Cannot manage sites. Reason: %s", e.getMessage());
+            logger.info(msg, e);
+            response.sendError(400, msg);
+        }
+        return null;
+    }
+
+    @ModelAttribute("allPages")
+    public Page<CmsPage> allPages(HttpServletRequest request, HttpServletResponse response,
+                                  @RequestParam(value = "page", defaultValue = "0") int page,
+                                  @RequestParam(value = "pageSize", defaultValue = "5") int pageSize) throws IOException {
+        try {
+            CmsUser cmsUser = registrationManager.findUser(request.getRemoteUser());
+            Pageable pageable = new PageRequest(page, pageSize, Sort.Direction.ASC, "id");
+            if (isWebmaster(cmsUser)) {
+                return pageManager.findAllPages(cmsUser, pageable);
+            } else if (isAuthor(cmsUser)) {
+                return pageManager.findAuthoredPages(cmsUser, pageable);
+            }
+        } catch (RegistrationException e) {
+            String msg = String.format("Cannot manage pages. Reason: %s", e.getMessage());
+            logger.info(msg, e);
+            response.sendError(400, msg);
+        }
+        return null;
+    }
+
+    @RequestMapping({"/pages"})
+    public String show(ModelMap model) {
+        model.put("cmsPage", new CmsPage());
+        model.put("mode", "add");
+        return "pages";
+    }
+
+    @RequestMapping(value = {"/pages"}, method = RequestMethod.POST)
+    public String addContent(@ModelAttribute("cmsPage") CmsPage cmsPage,
+                             final BindingResult bindingResult, final ModelMap model) throws IOException {
+        if (bindingResult.hasErrors()) {
+            return "pages";
+        }
+        pageManager.createPage(cmsPage);
+        model.clear();
+        return "redirect:/pages";
+    }
+
+    @RequestMapping(value = {"/pages/{pageId}"}, method = RequestMethod.DELETE)
+    public String delete(HttpServletResponse response, @PathVariable("pageId") String pageId) throws IOException {
+        pageManager.deletePage(pageId);
+        return "redirect:/pages";
+    }
+}
